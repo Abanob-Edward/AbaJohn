b50c8a5405e828c5dac92751f77460360c762ca1
Mody Medhat <105889951+Mody-Medhat@users.noreply.github.com>
2023-09-09 00:24:29 +0300

    AdminController.cs

--- AbaJohn/Controllers/AdminController.cs
+++ AbaJohn/Controllers/AdminController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Linq;
-
+// Mody Medhat
 namespace AbaJohn.Controllers
 {
    // [Authorize(Roles = "admin")]
